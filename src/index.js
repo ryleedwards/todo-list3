447cd0c75f58efbe0dfc388506532b87d9324754
Ryle Edwards <ryle.edwards@gmail.com>
2023-08-07 15:53:38 -0500

    Add placeholder project and todos creation for testing

--- src/index.js
+++ src/index.js
@@ -5,6 +5,14 @@ import './style.css';
 
 const myProjects = [];
 myProjects.push(new Project('My First Project'));
+myProjects.push(new Project('My Second Project'));
+myProjects[0].todos.push(
+  new Todo('lakjsdlkfj', 'kljslkfdjksldf', 'Aug 7', 'high')
+);
+myProjects[0].todos.push(
+  new Todo('kljsdflkj', 'jakshdfkjhsajkfdhkjdshf', 'Aug 9', 'low')
+);
 
 displayController.renderInitialLayout();
 displayController.populateProjects(myProjects);
+displayController.populateTodos(myProjects[0].todos);
