769f93d5bc3cd4450770973d10aa0bdab854f846
Ryle Edwards <ryle.edwards@gmail.com>
2023-08-08 17:09:41 -0500

    Insert testing for form handling in DOM

--- src/index.js
+++ src/index.js
@@ -9,10 +9,11 @@ myProjects.push(new Project('My Second Project'));
 myProjects[0].todos.push(
   new Todo('lakjsdlkfj', 'kljslkfdjksldf', 'Aug 7', 'high')
 );
-myProjects[0].todos.push(
+myProjects[0].addTodo(
   new Todo('kljsdflkj', 'jakshdfkjhsajkfdhkjdshf', 'Aug 9', 'low')
 );
 
 displayController.renderInitialLayout();
 displayController.populateProjects(myProjects);
 displayController.populateTodos(myProjects[0].todos);
+displayController.initClickHandlers();
